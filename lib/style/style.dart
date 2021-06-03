d650f3b06f3bc8de9d6b482a5074841e7f05e057
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2021-06-03 16:21:03 +0200

    Rename login style to button style

--- lib/style/style.dart
+++ lib/style/style.dart
@@ -3,8 +3,6 @@ import 'dart:ui';
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
-import 'constant.dart';
-
 const LABEL_STYLE = TextStyle(
   color: Colors.white,
   fontFamily: 'OpenSans',
@@ -17,21 +15,10 @@ const HINT_STYLE = TextStyle(
   fontFamily: 'OpenSans',
 );
 
-const LOGIN_STYLE = TextStyle(
+const BUTTON_STYLE = TextStyle(
   color: Color(0xFF425a7a),
   fontFamily: 'OpenSans',
   fontSize: 16,
   fontWeight: FontWeight.bold,
   letterSpacing: 1.5,
-);
-
-final boxDecorationStyle = BoxDecoration(
-  color: BACKGROUND_COLOR,
-  borderRadius: BorderRadius.circular(10),
-  boxShadow: [
-    BoxShadow(
-      color: Colors.black54,
-      blurRadius: 6,
-    ),
-  ],
 );
