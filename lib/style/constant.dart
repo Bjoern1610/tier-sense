e71d0e18edda50e3c93efdac42f9bb623b64277e
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2021-06-03 16:20:42 +0200

    Delete constant.dart

--- lib/style/constant.dart
+++
@@ -1,5 +0,0 @@
-import 'dart:ui';
-
-const Color BACKGROUND_COLOR = Color(0xFF425a7a);
-const String GOOGLE_URL = 'assets/graphics/google.png';
-const String FACEBOOK_URL = 'assets/graphics/facebook.png';
