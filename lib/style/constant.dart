a6372e13758c01c501e0e303a97e591558259094
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2021-06-03 13:26:59 +0200

    Create constant.dart

---
+++ lib/style/constant.dart
@@ -0,0 +1,5 @@
+import 'dart:ui';
+
+const Color BACKGROUND_COLOR = Color(0xFF425a7a);
+const String GOOGLE_URL = 'assets/graphics/google.png';
+const String FACEBOOK_URL = 'assets/graphics/facebook.png';
