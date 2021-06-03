3ee54aaad1fcbbbac81cf9ee8490eef187bf2fe7
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2021-06-03 12:46:59 +0200

    Create tier.dart

---
+++ lib/view/screens/tier/tier.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Tier extends StatefulWidget {
+
+  Tier({Key key}): super(key: key);
+
+  @override
+  _TierState createState() => _TierState();
+}
+
+class _TierState extends State<Tier> {
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold()
+    );
+  }
+}
