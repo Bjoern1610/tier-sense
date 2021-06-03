bbd6bfc75454ac528a9b7d39640b14e5dad43369
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2021-06-03 16:20:37 +0200

    Update package structure

--- lib/routes/routes.dart
+++ lib/routes/routes.dart
@@ -1,6 +1,6 @@
-import 'package:FoodSense/screens/login/login.dart';
-import 'package:FoodSense/screens/tier/tier.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:food_sense/screens/login/login.dart';
+import 'package:food_sense/screens/tier/tier.dart';
 
 class Routes {
 
