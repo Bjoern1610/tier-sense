b5ad3d6702f5bcd1f67e85dc1928d68948783e1e
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2021-06-03 12:46:56 +0200

    Create login.dart

---
+++ lib/view/screens/login/login.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+class Login extends StatefulWidget {
+
+  Login({Key key}): super(key: key);
+
+  @override
+  _LoginState createState() => _LoginState();
+}
+
+class _LoginState extends State<Login> {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
