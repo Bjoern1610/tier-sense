487af0d145257a79d792d380eccef03cd8d60db1
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2021-06-10 00:29:14 +0200

    Create colors.dart

---
+++ lib/screen/colors.dart
@@ -0,0 +1,5 @@
+import 'dart:ui';
+
+const BACKGROUND_BRIGHT_COLOR = Color.fromRGBO(136, 189, 188, 1);
+const BACKGROUND_MEDIUM_COLOR = Color.fromRGBO(37, 78, 88, 1);
+const BACKGROUND_DARK_COLOR = Color.fromRGBO(17, 45, 50, 1);
