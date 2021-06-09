c81a58d5681d61bcb94b4d0e408765553629b1ba
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2021-06-10 00:29:29 +0200

    Add styles

---
+++ lib/screen/styles.dart
@@ -0,0 +1,38 @@
+import 'dart:ui';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import 'colors.dart';
+
+const LOGIN_BUTTON_STYLE = TextStyle(
+  color: BACKGROUND_MEDIUM_COLOR,
+  fontFamily: 'OpenSans',
+  fontSize: 18,
+  fontWeight: FontWeight.bold,
+  letterSpacing: 1.5,
+);
+
+const NOPE_BUTTON_STYLE = TextStyle(
+  color: BACKGROUND_MEDIUM_COLOR,
+  fontFamily: 'OpenSans',
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+  letterSpacing: 1.5,
+);
+
+const SUPER_BUTTON_STYLE = TextStyle(
+  color: BACKGROUND_MEDIUM_COLOR,
+  fontFamily: 'OpenSans',
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+  letterSpacing: 1.5,
+);
+
+const LIKE_BUTTON_STYLE = TextStyle(
+  color: BACKGROUND_MEDIUM_COLOR,
+  fontFamily: 'OpenSans',
+  fontSize: 16,
+  fontWeight: FontWeight.bold,
+  letterSpacing: 1.5,
+);
