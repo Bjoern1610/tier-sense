1277437745804a404b6166549f8c08e84c9bf061
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2021-06-11 01:07:23 +0200

    Add swipe card text style

--- lib/screen/styles.dart
+++ lib/screen/styles.dart
@@ -5,6 +5,8 @@ import 'package:flutter/material.dart';
 
 import 'colors.dart';
 
+/* Text styles */
+
 const LOGIN_BUTTON_STYLE = TextStyle(
   color: BACKGROUND_MEDIUM_COLOR,
   fontFamily: 'OpenSans',
@@ -13,7 +15,7 @@ const LOGIN_BUTTON_STYLE = TextStyle(
   letterSpacing: 1.5,
 );
 
-const NOPE_BUTTON_STYLE = TextStyle(
+const NOPE_BUTTON_TEXT_STYLE = TextStyle(
   color: BACKGROUND_MEDIUM_COLOR,
   fontFamily: 'OpenSans',
   fontSize: 16,
@@ -21,7 +23,7 @@ const NOPE_BUTTON_STYLE = TextStyle(
   letterSpacing: 1.5,
 );
 
-const SUPER_BUTTON_STYLE = TextStyle(
+const SUPER_BUTTON_TEXT_STYLE = TextStyle(
   color: BACKGROUND_MEDIUM_COLOR,
   fontFamily: 'OpenSans',
   fontSize: 16,
@@ -29,10 +31,18 @@ const SUPER_BUTTON_STYLE = TextStyle(
   letterSpacing: 1.5,
 );
 
-const LIKE_BUTTON_STYLE = TextStyle(
+const LIKE_BUTTON_TEXT_STYLE = TextStyle(
   color: BACKGROUND_MEDIUM_COLOR,
   fontFamily: 'OpenSans',
   fontSize: 16,
+  fontWeight: FontWeight.bold,
+  letterSpacing: 1.5,
+);
+
+const ITEM_SWIPE_CARD_TEXT_STYLE = TextStyle(
+  color: BACKGROUND_MEDIUM_COLOR,
+  fontFamily: 'OpenSans',
+  fontSize: 42,
   fontWeight: FontWeight.bold,
   letterSpacing: 1.5,
 );
