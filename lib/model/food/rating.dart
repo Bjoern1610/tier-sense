30e4e806b8b636379b3c3483a2367c6b39e67c59
Björn Holtvogt <bjoern.holtvogt@gmail.com>
2021-06-11 22:23:52 +0200

    Add duplicate query

--- lib/model/food/rating.dart
+++ lib/model/food/rating.dart
@@ -13,18 +13,24 @@ class Rating {
   List<String> get nopes => List.unmodifiable(_nopes);
 
   void addNope(String nope) {
-    _nopes.add(nope);
+    if (!_nopes.contains(nope)) {
+      _nopes.add(nope);
+    }
   }
 
   List<String> get supers => List.unmodifiable(_supers);
 
   void addSuper(String superLike) {
-    _supers.add(superLike);
+    if (!_supers.contains(superLike)) {
+      _supers.add(superLike);
+    }
   }
 
   List<String> get likes => List.unmodifiable(_likes);
 
   void addLike(String like) {
-    _likes.add(like);
+    if (!_likes.contains(like)) {
+      _likes.add(like);
+    }
   }
 }
